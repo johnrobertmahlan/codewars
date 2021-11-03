0ed8b420c280ee29d35576d3b84e0b253e60890b
John Robert Mahlan <johnrobertmahlan@gmail.com>
2021-11-03 00:06:07 -0600

    update to correct solution

--- find_the_missing_letter/find_the_missing_letter.rb
+++ find_the_missing_letter/find_the_missing_letter.rb
@@ -1,8 +1,12 @@
-ALPHABET = ("a".."z").to_a
+ALPHABETUP = ("A".."Z").to_a
+ALPHABETDOWN = ("a".."z").to_a
 
 def find_missing_letter(arr)
-  subalpha = ALPHABET.select {|letter| arr.include?(letter)}
-  ((subalpha.first..subalpha.last).to_a - subalpha)[0]
+  subalpha = determine_alphabet(arr[0]).select {|letter| arr.include?(letter)}
+  diff = (subalpha.first..subalpha.last).to_a
+  (diff - subalpha)[0]
 end
 
-p find_missing_letter(["a","b","c","d","f"])
+def determine_alphabet(letter)
+  ALPHABETUP.include?(letter) ? ALPHABETUP : ALPHABETDOWN
+end
